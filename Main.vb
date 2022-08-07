fb64c395264cdb0d571926eb604db7f3ae779eb1
Bobtastisch <e.jerber@web.de>
2022-08-07 12:15:18 +0200

    Read to Realies

--- Main.vb
+++ Main.vb
@@ -3,12 +3,12 @@
     Sub Main()
         CreatePasswortFile.Textfile()
 
-        'StreamAufruf.Aufruf("wubbl0rz")                    'get Livestreamer that is live and give back ID of Streamer
+        'StreamAufruf.Aufruf("bobtastisch2")                    'get Livestreamer that is live and give back ID of Streamer
         FollowersFromUser.Aufruf(187244607)
-        'LiveStreamerAufruf.Aufruf(231641204)
-        'FollowersFromUser.Aufruf(StreamAufruf.StreamIDgiveback)       'get Your followers
+        'LiveStreamerAufruf.Aufruf(187244607)
+        'FollowersFromUser.Aufruf(StreamAufruf.StreamIDgiveback)'get Your followers
         For Each ID In GetFollowersFromUser.Endliste
-            LiveStreamerAufruf.Aufruf(ID)              'get all streamstats  from live streamers need to be live
+            LiveStreamerAufruf.Aufruf(ID)                       'get all streamstats  from live streamers need to be live
         Next
         Twitch.ConnectAufruf.Aufruf()
         Console.ReadLine()
